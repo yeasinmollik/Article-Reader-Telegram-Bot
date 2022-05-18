a9835742ebc768764f0cf21df0323b0738d28ec3
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-05-18 20:28:17 +0600

    Implemented get_telegraph_url function

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import logging
 import json
 import subprocess
 import time
+
+import requests
 from telegram import (Update,
-                      ReplyKeyboardMarkup, ReplyKeyboardRemove, ChatAction)
+                      ReplyKeyboardMarkup, ReplyKeyboardRemove, ChatAction, MessageEntity)
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -12,6 +14,7 @@ from telegram.ext import (
     Filters,
     ConversationHandler,
 )
+import export_to_telegraph
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,6 +24,21 @@ def start(update: Update, context: CallbackContext):
     update.message.reply_text('Greetings! Welcome to Article Reader Bot')
 
 
+def unshorten_url(url):
+    return requests.get(url).url
+
+
+def get_telegraph_url(update: Update, context: CallbackContext):
+    url = update.message.text
+    # url = unshorten_url(url) if "link.medium.com" in url else url  # unshorten medium.com urls
+    # print(url)
+
+
+    m_id = update.message.reply_text('Please wait while I fetch the article...', quote=True).message_id
+    telegraph_url = export_to_telegraph.export(url, force=True)
+    context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=m_id, text=telegraph_url)
+
+
 def main():
     with open('config.json') as config_file:
         config = json.load(config_file)
@@ -28,7 +46,11 @@ def main():
     dispatcher = updater.dispatcher
 
     start_handler = CommandHandler('start', start)
+    telegraph_handler = MessageHandler(Filters.text & (Filters.entity('url') | Filters.entity(MessageEntity.TEXT_LINK)),
+                                       get_telegraph_url)
+
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(telegraph_handler)
     updater.start_polling()
 
 
