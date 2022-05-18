044991e10c57bfd714c6c0e255aa3b098d9b3ff6
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-05-19 03:21:46 +0600

    Implemented text2speech function
    and also added some improvements to get_telegraph_url function

--- main.py
+++ main.py
@@ -15,9 +15,16 @@ from telegram.ext import (
     ConversationHandler,
 )
 import export_to_telegraph
+import azure.cognitiveservices.speech as speechsdk
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+export_to_telegraph.token = config['telegraph_token']
+speech_config = speechsdk.SpeechConfig(subscription=config['subscription'], region=config['region'])
+speech_config.set_speech_synthesis_output_format(speechsdk.SpeechSynthesisOutputFormat.Audio16Khz32KBitRateMonoMp3)
 
 
 def start(update: Update, context: CallbackContext):
@@ -26,6 +33,10 @@ def start(update: Update, context: CallbackContext):
 
 def unshorten_url(url):
     return requests.get(url).url
+
+
+def instant_view(url):
+    return "https://" + export_to_telegraph.export(url, force=True, noSourceLink=True)
 
 
 def get_telegraph_url(update: Update, context: CallbackContext):
@@ -40,15 +51,23 @@ def get_telegraph_url(update: Update, context: CallbackContext):
         url = re.sub(r'^.*?.com', 'https://scribe.rip', url)
 
     m_id = update.message.reply_text('Fetching the article...').message_id
-    telegraph_url = export_to_telegraph.export(url, force=True, noSourceLink= True)
-    context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-    telegraph_url = f"https://{telegraph_url}" if "https://" not in telegraph_url else telegraph_url
+    # context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+    telegraph_url = instant_view(url)
     context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=m_id, text=telegraph_url)
 
 
+def get_text2speech(text, title):
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=f"{title}.mp3")
+    speech_config.speech_synthesis_voice_name = 'en-US-ChristopherNeural'
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+
+
+def text2speech(update: Update, context: CallbackContext, title):
+    url = update.message.text
+
+
 def main():
-    with open('config.json') as config_file:
-        config = json.load(config_file)
     updater = Updater(token=config['tg_token'], use_context=True)
     dispatcher = updater.dispatcher
 
